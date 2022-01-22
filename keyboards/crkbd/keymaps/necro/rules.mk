bb294196de9a50e4912e4e37522dff0347c4ec32
Armik <armik@pigarmik.com>
2022-01-21 20:36:25 -0800

    Add files via upload

---
+++ keyboards/crkbd/keymaps/necro/rules.mk
@@ -0,0 +1,15 @@
+EXTRAFLAGS += -flto
+
+RGB_MATRIX_ENABLE = WS2812
+OLED_ENABLE = yes
+LOCAL_GLCDFONT = yes
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  glcdfont.c \
+        ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
