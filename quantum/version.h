a41cceb5c9a3c8431d78c8316bbae6e1b89ea65d
Necronar <armik@pigarmik.com>
2022-01-23 01:37:52 -0800

    Alex_RGB

---
+++ quantum/version.h
@@ -0,0 +1,9 @@
+/* This file was automatically generated. Do not edit or copy.
+ */
+
+#pragma once
+
+#define QMK_VERSION "0.8.133-6789-gcb9068-dirty"
+#define QMK_BUILDDATE "2022-01-22-21:07:27"
+#define CHIBIOS_VERSION "develop_2021_q4"
+#define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-8-gd1c212"
